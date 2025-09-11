fa2f06e2e130f5adf8bf02537319a71c89cbbe53
jtenshix <jtenshix@gmail.com>
2025-09-11 16:02:34 -0300

    Update google.js

--- api/google.js
+++ api/google.js
@@ -2,7 +2,6 @@ import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
   try {
-    // ID de la playlist que me proporcionaste.
     const playlistId = 'PLkcZMdABSTfhe26NpsiFJev_0lCOcfCCf';
     const response = await fetch(
       `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails&playlistId=${playlistId}&maxResults=50&key=${process.env.API_KEY}`
