1d2fc11dd8ea22f121d1c9851b6d8ec60218375c
jtenshix <jtenshix@gmail.com>
2025-09-11 13:47:39 -0300

    Create google.js

---
+++ api/google.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+  try {
+    const q = req.query.q || "Tenshi"; // lo que quieres buscar
+    const response = await fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(q)}&key=${process.env.GOOGLE_API_KEY}`
+    );
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
