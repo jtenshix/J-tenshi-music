d8f5ce8054cf6ec28267d2dfbf16967f15891db1
jtenshix <jtenshix@gmail.com>
2025-09-11 14:19:34 -0300

    Update google.js

--- api/google.js
+++ api/google.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   try {
     const q = req.query.q || "Tenshi"; // lo que quieres buscar
     const response = await fetch(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(q)}&key=${process.env.GOOGLE_API_KEY}`
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(q)}&key=${process.env.API_KEY}`
     );
     const data = await response.json();
     res.status(200).json(data);
